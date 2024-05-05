aaf0a836664466bdb8a31c08cee5cdc297c42165
soumi purkayastha <125696162+soumi-eee@users.noreply.github.com>
2024-05-06 00:32:33 +0530

    Add files via upload

---
+++ contactmanagement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ class contact{
+  private String name;
+  private int contactnum;
+  private String email;
+  public contact(String name,int contactnum,String email){
+    this.name=name;
+    this.contactnum=contactnum;
+    this.email=email;
+  }
+}
+Hashmap<in
+ 
+public class contactmanagement {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("name of the contact ");
+    String contact = sc.next();
+    System.out.println("number of the contact");
+    int num = sc.nextInt();
+    System.out.println("enter the email of the contact");
+    String email = sc.next();
+    System.out.println("do you want to view your contact list");
+    
+  }  
+}
